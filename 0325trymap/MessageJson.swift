e9c6718538b6b03bd52781adb2586f4f874295d4
lincircle <cosine@cgua.local>
2016-04-14 22:53:17 +0800

    新增messageJson類別
    因為從server回傳json格式後需要

---
+++ 0325trymap/MessageJson.swift
@@ -0,0 +1,18 @@
+//
+//  MessageJson.swift
+//  0325trymap
+//
+//  Created by cosine on 2016/4/14.
+//  Copyright © 2016年 Lin Circle. All rights reserved.
+//
+
+import Foundation
+
+class MessageJson {
+    
+    var message: String = ""
+    var latitude: Double = 0
+    var longitude: Double = 0
+    var date: NSDate? = nil
+    
+}
